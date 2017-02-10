1a4c8d37cda74f56f95a1ee739feeda3a479a668
Samsil Arefin <samsilarefin000@gmail.com>
2017-02-10 22:17:47 +0600

    Add files via upload

---
+++ 766A.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstring>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    string str1,str2;
+    cin >> str1 >> str2;
+    if (str1==str2)
+    cout << -1;
+    else
+    cout << max(str1.size(),str2.size());
+    return 0;
+}
---
+++ 766B.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstring>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+    	cin>>a[i];
+	}
+	sort(a,a+n);
+	for (int i=1;i<n-1;i++)
+    {
+        if (a[i-1]+a[i]>a[i+1])
+        {
+            cout << "YES";
+            return 0;
+        }
+    }
+    cout << "NO";
+}
