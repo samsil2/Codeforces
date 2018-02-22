947cff5b02727452e60ce7eb95b82f9a6ceb3df2
Samsil Arefin <samsilarefin000@gmail.com>
2018-02-23 02:41:00 +0600

    Add files via upload

---
+++ 913A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{   //procodist.com
+	
+	 ios_base::sync_with_stdio(0);
+	 cin.tie(NULL);
+	  long long n,m,ans,p;
+    cin>>n>>m;
+     p=pow(2,n);
+     ans=m%p;
+     cout<<ans<<endl;
+     
+}
